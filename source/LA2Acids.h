82de9f9afbdbefcf96b501fa79516dc3ec1174ab
Rhys Smith <rsmith@sd22.bc.ca>
2026-04-13 10:48:46 -0700

    Inital Commit

---
+++ source/LA2Acids.h
@@ -0,0 +1,18 @@
+//------------------------------------------------------------------------
+// Copyright(c) 2026 My Plug-in Company.
+//------------------------------------------------------------------------
+
+#pragma once
+
+#include "pluginterfaces/base/funknown.h"
+#include "pluginterfaces/vst/vsttypes.h"
+
+namespace MyCompanyName {
+//------------------------------------------------------------------------
+static const Steinberg::FUID kLA2A_CompressorProcessorUID (0x3E2117D2, 0x8482586B, 0x8A7C0F0A, 0xD3C3EDC3);
+static const Steinberg::FUID kLA2A_CompressorControllerUID (0x776B35EC, 0x74FF5454, 0x9EC633A1, 0x62FF29BD);
+
+#define LA2A_CompressorVST3Category "Fx"
+
+//------------------------------------------------------------------------
+} // namespace MyCompanyName
---
+++ source/LA2Aentry.cpp
@@ -0,0 +1,50 @@
+//------------------------------------------------------------------------
+// Copyright(c) 2026 My Plug-in Company.
+//------------------------------------------------------------------------
+
+#include "LA2Aprocessor.h"
+#include "LA2Acontroller.h"
+#include "LA2Acids.h"
+#include "version.h"
+
+#include "public.sdk/source/main/pluginfactory.h"
+
+#define stringPluginName "LA2A_Compressor"
+
+using namespace Steinberg::Vst;
+using namespace MyCompanyName;
+
+//------------------------------------------------------------------------
+//  VST Plug-in Entry
+//------------------------------------------------------------------------
+
+BEGIN_FACTORY_DEF ("My Plug-in Company", 
+			       "https://www.mycompanyname.com", 
+			       "mailto:info@mycompanyname.com")
+
+	//---First Plug-in included in this factory-------
+	// its kVstAudioEffectClass component
+	DEF_CLASS2 (INLINE_UID_FROM_FUID(kLA2A_CompressorProcessorUID),
+				PClassInfo::kManyInstances,	// cardinality
+				kVstAudioEffectClass,	// the component category (do not changed this)
+				stringPluginName,		// here the Plug-in name (to be changed)
+				Vst::kDistributable,	// means that component and controller could be distributed on different computers
+				LA2A_CompressorVST3Category, // Subcategory for this Plug-in (to be changed)
+				FULL_VERSION_STR,		// Plug-in version (to be changed)
+				kVstVersionString,		// the VST 3 SDK version (do not changed this, use always this define)
+				LA2A_CompressorProcessor::createInstance)	// function pointer called when this component should be instantiated
+
+	// its kVstComponentControllerClass component
+	DEF_CLASS2 (INLINE_UID_FROM_FUID (kLA2A_CompressorControllerUID),
+				PClassInfo::kManyInstances, // cardinality
+				kVstComponentControllerClass,// the Controller category (do not changed this)
+				stringPluginName "Controller",	// controller name (could be the same than component name)
+				0,						// not used here
+				"",						// not used here
+				FULL_VERSION_STR,		// Plug-in version (to be changed)
+				kVstVersionString,		// the VST 3 SDK version (do not changed this, use always this define)
+				LA2A_CompressorController::createInstance)// function pointer called when this component should be instantiated
+
+	//----for others Plug-ins contained in this factory, put like for the first Plug-in different DEF_CLASS2---
+
+END_FACTORY
